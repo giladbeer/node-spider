7d68a77a104cfa2dee7e7e34ea5ad603535654ce
giladbeer <gilad.beer@gmail.com>
2022-11-30 20:20:37 +1300

    update: move more options to config, add algolia backup

--- src/configLoader.ts
+++ src/configLoader.ts
@@ -1,7 +1,8 @@
 import * as fs from 'fs';
+import { CrawlSiteOptionsCrawlerConfig } from './types';
 
 export const loadConfig = (filePath: string) => {
   const configFile = fs.readFileSync(filePath);
   const jsonConfig = JSON.parse(configFile.toString());
-  return jsonConfig;
+  return jsonConfig as CrawlSiteOptionsCrawlerConfig;
 };
